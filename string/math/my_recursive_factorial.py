3d363497e2d84547cb8db3cfa122f89bdd1d8973
Abdulla Abdukulov <abduqulovabdulla3108@gmail.com>
2022-11-08 11:34:09 +0500

    I have done Math

---
+++ string/math/my_recursive_factorial.py
@@ -0,0 +1,11 @@
+def my_recursive_factorial(n):
+    '''
+    This is a recursive function
+    to find the factorial of an integer
+    '''
+    if n==1:
+        return 1
+    else:
+        # recursive call to the function
+        return (n* my_recursive_factorial(n-1))
+print(my_recursive_factorial(5))
