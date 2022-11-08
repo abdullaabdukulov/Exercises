eebb1587c1439426d68acf0ae87bbcbf72e28be8
Abdulla Abdukulov <abduqulovabdulla3108@gmail.com>
2022-11-08 11:35:40 +0500

    I have done Math

--- math/python/my_recursive_factorial.py
+++ math/python/my_recursive_factorial.py
@@ -8,4 +8,4 @@ def my_recursive_factorial(n):
     else:
         # recursive call to the function
         return (n* my_recursive_factorial(n-1))
-print(my_recursive_factorial(5))
+# print(my_recursive_factorial(5))
